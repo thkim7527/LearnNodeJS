b2c6cc4a0b04610a983a07b184f1c0c3c87a291c
thkim7527 <thkim7527@gmail.com>
2021-08-24 23:19:36 +0900

    Delete all html

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var query = url.parse(_url, true).query;
-    var title = query.id;
-
-    if(_url == '/'){
-        title = 'Welcome';
+//ToDo
+const indexList = fs.readdir("./data", (err, fileList) => {
+    let indexList = `<ul>`;
+    for(i=0; i < fileList.length; i++) {
+        indexList += `<li><a href="?id=${fileList[i]}">${fileList[i]}</a></li>`;
     }
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
-    }
+    indexList += `</ul>`
+    
+    return indexList;
+});
 
-    var template = `
+function templateHTML(title) {
+    fs.readFile(`./data/${title}`, (err, description) => {
+        const template = `
         <!DOCTYPE html>
         <html>
             <head>
@@ -22,38 +23,42 @@ var app = http.createServer(function(request,response){
                 <title>Web</title>
                 <link rel="stylesheet" href="style.css">
             </head>
-        
+    
             <body>
                 <h1><a href="/">Web</a></h1>
-        
-                <div id="grid">
-                    <div id="index">
-                        <ol>
-                            <li><a href="?id=HTML">HTML</a></li>
-                            <li><a href="?id=CSS">CSS</a></li>
-                            <li><a href="?id=JavaScript">JavaScript</a></li>
-                        </ol>
-                    </div>
-        
-                    <div id="article">
-                        <h2>${title}</h2>
-                        <button onclick="toggleDarkmode()">Darkmode</button>
-        
-                        <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="HTML Speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for creating web pages and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.</p>
-        
-                        <img src="HTML.jpg" width="500px">
-        
-                        <p>HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.</p>
-                    </div>
-                </div>
-        
+                ${indexList}
+                <h2>${title}</h2>
+                <button onclick="toggleDarkmode()">Darkmode</button>
+                ${description}
                 <script src="script.js"></script>
             </body>
-        </html>    
-    `;
+        </html>
+        `;
+        return template;
+    });
+}
 
-    response.writeHead(200);
-    response.end(template);
+const app = http.createServer(function(request,response){
+    const _url = request.url;
+    const query = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+    let title = query.id;
+
+    if(pathname === '/') {
+        if(title === undefined) {
+            title = 'Web';
+            const template = templateHTML(title);    
+            response.writeHead(200);
+            response.end(template);
+        } else {
+            const template = templateHTML(title);
+            response.writeHead(200);
+            response.end(template);
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not Found');
+    }
 });
 
 app.listen(80);
