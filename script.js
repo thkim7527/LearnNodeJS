7d9254ec5f52d4687d703bde965caf1c5a4c7d8a
thkim7527 <thkim7527@gmail.com>
2021-09-08 12:42:13 +0900

    Delete script and style files

--- script.js
+++
@@ -1,5 +0,0 @@
-function toggleDarkmode() {
-    document.body.classList.toggle("darkmode");
-    document.querySelector("h1").classList.toggle("darkmode");
-    document.querySelector("#index").classList.toggle("darkmode");
-}
