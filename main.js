62bfaa445d9aa9eb07692cbf54947a8474c4139b
thkim7527 <thkim7527@gmail.com>
2021-10-12 12:32:17 +0900

    Add Create

--- main.js
+++ main.js
@@ -1,71 +1,126 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const qs = require('querystring')
 
-function templateHTML(indexList, title, description) {
-    const template = `
+function getTemplate(title, list, body) {
+    return `
         <!DOCTYPE html>
-        <html>
+        <html lang="en">
             <head>
                 <meta charset="utf-8">
                 <title>Web</title>
-                <link rel="stylesheet" href="style.css">
             </head>
-    
+
             <body>
                 <h1><a href="?id=Web">Web</a></h1>
-                ${indexList}
+                ${list}
+                <a href="/create">Create</a>
                 <h2>${title}</h2>
-                ${description}
-                <script src="script.js"></script>
+                ${body}
             </body>
         </html>
-    `;
-    return template
+    `
 }
 
-const app = http.createServer(function(request,response){
+function getList(fileList) {
+    let listHTML = `<ul>`;
+    for(i=0; i < fileList.length; i++) {
+        listHTML += `<li><a href="?id=${fileList[i]}">${fileList[i]}</a></li>`
+    }
+    listHTML += `</ul>`
+
+    return listHTML
+}
+
+const app = http.createServer(function(request, response){
     const _url = request.url;
     const query = url.parse(_url, true).query;
     const pathname = url.parse(_url, true).pathname;
     let title = query.id;
 
-    if(pathname === '/') {
-        if(pathname === undefined) {
+    switch(pathname) {
+        case "/":
+            if(title === undefined) {
+                fs.readdir("./data", (err, fileList) => {
+                    fs.readFile(`./data/${title}`, (err, description) => {
+                        response.writeHead(200);
+                        response.end(getTemplate("Web", getList(fileList), description));
+                    })
+                })
+            } else {
+                fs.readdir("./data", (err, fileList) => {
+                    fs.readFile(`./data/${title}`, (err, description) => {
+                        response.writeHead(200);
+                        response.end(getTemplate(title, getList(fileList), description));
+                    })
+                })
+            }
+            break;
+        case "/create":
             fs.readdir("./data", (err, fileList) => {
-                let indexList = `<ul>`;
-                for(i=0; i < fileList.length; i++) {
-                    indexList += `<li><a href="?id=${fileList[i]}">${fileList[i]}</a></li>`
-                }
-                indexList += `</ul>`
-
-                title = "Web"
-
-                fs.readFile(`./data/${title}`, (err, description) => {
-                    const template = templateHTML(indexList, title, description);    
-                    response.writeHead(200);
-                    response.end(template);
-                })  
+                const description = `
+                    <form method="post" action="/submit">
+                        <input type="text" name="title" placeholder="Title"/>
+                        <input type="text" name="description" placeholder="Description"/>
+                        <input type="submit"/>
+                    <form/>
+                `
+                response.writeHead(200);
+                response.end(getTemplate("Create", getList(fileList), description))
             })
-        } else {
-            fs.readdir("./data", (err, fileList) => {
-                let indexList = `<ul>`;
-                for(i=0; i < fileList.length; i++) {
-                    indexList += `<li><a href="?id=${fileList[i]}">${fileList[i]}</a></li>`
-                }
-                indexList += `</ul>`
+            break;
+        case "/submit":
+            let query = '';
+            request.on('data', function(data){
+                query += data;
+            });
+            request.on('end', function(){
+                let post = qs.parse(query);
+                let title = post.title;
+                let description = post.description;
 
-                fs.readFile(`./data/${title}`, (err, description) => {
-                    const template = templateHTML(indexList, title, description);    
-                    response.writeHead(200);
-                    response.end(template);
-                })  
-            })
-        }
-    } else {
-        response.writeHead(404);
-        response.end('Not Found');
+                console.log(title)
+                console.log(description)
+            });
+
+            response.writeHead(200)
+            response.end(getTemplate("Create Success", "", ""))
+            break;
+        default:
+            response.writeHead(404);
+            response.end('Not Found');
     }
+
+    // if(pathname === '/') {
+    //     if(pathname === undefined) {
+    //         fs.readdir("./data", (err, fileList) => {
+    //             title = "Web"
+    //             const list = getList(fileList)
+    //
+    //             fs.readFile(`./data/${title}`, (err, description) => {
+    //                 response.writeHead(200);
+    //                 response.end(getTemplate(title, list, description));
+    //             })
+    //         })
+    //     } else {
+    //         fs.readdir("./data", (err, fileList) => {
+    //             const list = getList(fileList)
+    //
+    //             fs.readFile(`./data/${title}`, (err, description) => {
+    //                 response.writeHead(200);
+    //                 response.end(getTemplate(title, list, description));
+    //             })
+    //         })
+    //     }
+    // } else if(pathname === '/create') {
+    //     fs.readdir("./data", (err, fileList) => {
+    //
+    //     })
+    // } else {
+    //     response.writeHead(404);
+    //     response.end('Not Found');
+    // }
 });
 
 app.listen(80);
